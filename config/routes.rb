1aab0df26fc6f7fb8f37aa714eb980db1dfdf378
Matthew Quan <mgquan@myseneca.ca>
2016-12-22 17:20:28 -0500

    Add a static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #the default web page should go to the application controller and use goodbye method
   root 'application#hello'
